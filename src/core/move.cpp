a5d002ba2f6b5aa650ea53a72e4d4815a1022ec7
Damien Hood <damien.hood@hotmail.com>
2025-02-05 11:04:10 -0500

    fix move, add uci translation, add ostream printing

--- src/core/move.cpp
+++ src/core/move.cpp
@@ -15,13 +15,41 @@ namespace chess {
     Square Move::getTo() const { return static_cast<Square>((moveData >> 6) & 0x3F); }
     uint8_t Move::getFlags() const { return static_cast<uint8_t>((moveData >> 12) & 0xF); }
 
-    bool Move::isCapture() const { return getFlags() & 0b0001'0000; } // Bit 4
-    bool Move::isPromotion() const { return getFlags() & 0b1000; }    // Bit 3
+    bool Move::isQuiet() const { return getFlags() == 0; }            // Flag 0 = Quiet move
+    bool Move::isCapture() const { return getFlags() & 0b0100; } 
+    bool Move::isPromotion() const { return getFlags() & 0b1000; }    // 
     bool Move::isEnPassant() const { return getFlags() == 5; }        // Flag 5 = En passant
     bool Move::isCastle() const { return getFlags() == 2 || getFlags() == 3; }
     
     MoveCode Move::getMoveCode() const { return static_cast<MoveCode>(getFlags()); }
 
+    std::string Move::toUCIString() const {
+        MoveCode code = getMoveCode();
+        std::string move;
+        move += static_cast<char>('a' + enumToInt(getFrom()) % 8);
+        move += static_cast<char>('1' + enumToInt(getFrom()) / 8);
+        move += static_cast<char>('a' + enumToInt(getTo()) % 8);
+        move += static_cast<char>('1' + enumToInt(getTo()) / 8);
 
+        if (isPromotion()) {
+            switch (code) {
+                case MoveCode::KNIGHT_PROMO: move += 'n'; break;
+                case MoveCode::BISHOP_PROMO: move += 'b'; break;
+                case MoveCode::ROOK_PROMO: move += 'r'; break;
+                case MoveCode::QUEEN_PROMO: move += 'q'; break;
+                default: break;
+            }
+        }
+
+        return move;
+    }
+
+    std::ostream& operator<<(std::ostream& os, const Move& move){
+        std::string st = move.toUCIString();
+        st += " ";
+        st += std::to_string(enumToInt(move.getMoveCode()));
+        os << st;
+        return os;
+    }
 
 }
--- src/core/move.h
+++ src/core/move.h
@@ -1,17 +1,14 @@
+#pragma once
+
 #include <cstdint>  // Standard integer types
 #include <array>
 #include <string>
-#include "core/board.h"
+#include "core/definitions.h"
 
 namespace chess {
 
 // Move class to store move information while I get things going
 // TODO: Use uint16_t for move representation for better performance
-
-// Move codes from nibble flags
-enum class MoveCode : uint8_t {
-    QUIET, DOUBLE_PAWN_PUSH, KING_CASTLE, QUEEN_CASTLE, CAPTURE, EN_PASSANT, KNIGHT_PROMO, BISHOP_PROMO, ROOK_PROMO, QUEEN_PROMO, KNIGHT_PROMO_CAPTURE, BISHOP_PROMO_CAPTURE, ROOK_PROMO_CAPTURE, QUEEN_PROMO_CAPTURE
-};
 
 class Move{
     public:
@@ -23,6 +20,7 @@ class Move{
         Square getTo() const;
         uint8_t getFlags() const;
 
+        bool isQuiet() const;
         bool isCapture() const; 
         bool isPromotion() const; 
         bool isEnPassant() const; 
@@ -30,6 +28,10 @@ class Move{
 
         MoveCode getMoveCode() const;
 
+        std::string toUCIString() const;
+
+        friend std::ostream& operator<<(std::ostream& os, const Move& move);
+    
     private:
         uint16_t moveData;
 
