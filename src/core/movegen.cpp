06c79b887b9551230f28fb980c74c31f6c65ffdc
Damien Hood <damien.hood@hotmail.com>
2025-02-05 11:05:48 -0500

    add pseudoMove Move vector generation

--- src/core/movegen.cpp
+++ src/core/movegen.cpp
@@ -3,12 +3,44 @@
 #include <iostream>
 #include <bit>
 
+namespace{
+    // From Nihar Karve - much cleaner way of implemententing hyperbola quintessence
+    uint64_t hyp_quint(uint64_t pieceMask, uint64_t occ, uint64_t lineMask) {
+        return (((lineMask & occ) - pieceMask * 2) ^
+            reverse(reverse(lineMask & occ) - reverse(pieceMask) * 2)) & lineMask;
+    }
+}
+
 namespace chess {
 
 namespace Movegen {
 
+    std::vector<Move> getPseudoMoves(const Board &board, Color color){
+        uint64_t occupied = board.getOccupancy(color);
+        std::vector<Move> allMoves;
+        for(int i=0; i<64; i++){
+            if(getBit(occupied, i)){
+                Square square = static_cast<Square>(i);
+                std::optional<PieceType> pieceOpt = board.getPieceType(square);
+                if(!pieceOpt.has_value()){
+                    continue;
+                }
+                PieceType piece = pieceOpt.value();
+                uint64_t pseudo = pseudoLegal(board, square);
+                std::vector<Move> moves = bitboardToMoves(board, piece, square, pseudo);
+                allMoves.insert(allMoves.end(), moves.begin(), moves.end());
+            }
+        }
+        return allMoves;
+    }
+
     uint64_t pseudoLegal(const Board &board, Square square){
-        switch(board.getPieceType(square)){
+        std::optional<PieceType> pieceOpt = board.getPieceType(square);
+        if(!pieceOpt.has_value()){
+            return 0ULL;
+        }
+        PieceType piece = pieceOpt.value();
+        switch(piece){
             using enum PieceType;
             case WHITE_PAWN:
             case BLACK_PAWN:
@@ -105,13 +137,86 @@ namespace Movegen {
         return bishopPseudo(board, square) | rookPseudo(board, square);
     }
 
-    // From Nihar Karve - much cleaner way of implemententing hyperbola quintessence
-    uint64_t hyp_quint(uint64_t pieceMask, uint64_t occ, uint64_t lineMask) {
-        return (((lineMask & occ) - pieceMask * 2) ^
-            reverse(reverse(lineMask & occ) - reverse(pieceMask) * 2)) & lineMask;
+    std::vector<Move> bitboardToMoves(const Board &board, PieceType piece, Square square, const uint64_t& bitboard){
+        std::vector<Move> moves;
+        for(int i=0; i<64; i++){
+            if(getBit(bitboard, i)){
+                if(piece == PieceType::WHITE_PAWN && getRank(square) == Rank::RANK_7){ // White pawn promotion
+                    moves.push_back(buildMove(board, piece, square, static_cast<Square>(i), PieceType::WHITE_QUEEN));
+                    moves.push_back(buildMove(board, piece, square, static_cast<Square>(i), PieceType::WHITE_ROOK));
+                    moves.push_back(buildMove(board, piece, square, static_cast<Square>(i), PieceType::WHITE_BISHOP));
+                    moves.push_back(buildMove(board, piece, square, static_cast<Square>(i), PieceType::WHITE_KNIGHT));
+                } else if(piece == PieceType::BLACK_PAWN && getRank(square) == Rank::RANK_2){ // Black pawn promotion
+                    moves.push_back(buildMove(board, piece, square, static_cast<Square>(i), PieceType::BLACK_QUEEN));
+                    moves.push_back(buildMove(board, piece, square, static_cast<Square>(i), PieceType::BLACK_ROOK));
+                    moves.push_back(buildMove(board, piece, square, static_cast<Square>(i), PieceType::BLACK_BISHOP));
+                    moves.push_back(buildMove(board, piece, square, static_cast<Square>(i), PieceType::BLACK_KNIGHT));
+                } else {
+                    moves.push_back(buildMove(board, piece, square, static_cast<Square>(i), std::nullopt)); // Normal move
+                }
+            }
+        }
+        return moves;
     }
 
+    Move buildMove(const Board &board, PieceType piece, Square from, Square to, std::optional<PieceType> promotion){
+        uint8_t flags = 0;
+        // Check if a piece is captured
+        std::optional<PieceType> captured = board.getPieceType(to);
+        if(captured.has_value()){
+            flags |= 0b0100;
+        }
+        // Check if a pawn is promoted
+        if(promotion.has_value()){
+            flags |= 0b1000;
+            switch(promotion.value()){
+                using enum PieceType;
+                case WHITE_BISHOP: 
+                case BLACK_BISHOP:
+                    flags |= 0b0001; break;
+                case WHITE_ROOK:
+                case BLACK_ROOK: 
+                    flags |= 0b0010; break;
+                case WHITE_QUEEN: 
+                case BLACK_QUEEN: 
+                    flags |= 0b0011; break;
+                default: break;
+            }
+        }
 
+        // Check if a pawn is making a double push
+        if((piece == PieceType::WHITE_PAWN || piece == PieceType::BLACK_PAWN) && !promotion.has_value()){
+            // Check if going to en passant square
+            if(to == board.getEnPassantSquare()){
+                flags = 0b0101;
+            }
+        }
+
+        // Check if a pawn is making an en passant move
+        if(piece == PieceType::WHITE_PAWN || piece == PieceType::BLACK_PAWN){
+            if(board.getPieceType(to).has_value() && board.getPieceType(to).value() == PieceType::WHITE_PAWN){
+                flags = 0b0101;
+            }
+        }
+
+        // check if castling
+        if(piece == PieceType::WHITE_KING || piece == PieceType::BLACK_KING){
+            if(from == Square::E1 && to == Square::G1){
+                flags = 0b0010;
+            } else if(from == Square::E1 && to == Square::C1){
+                flags = 0b0011;
+            } else if(from == Square::E8 && to == Square::G8){
+                flags = 0b0010;
+            } else if(from == Square::E8 && to == Square::C8){
+                flags = 0b0011;
+            }
+        }
+      
+        return Move(from, to, flags);
+    }
+    
+
+    
 }
 
 }
