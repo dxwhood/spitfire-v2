15e863e6504a5cf3c1bd0079ffdbf2f4c98d8204
Damien Hood <damien.hood@hotmail.com>
2025-02-06 20:15:33 -0500

    fix castling in pseudo Move generation

--- src/core/movegen.cpp
+++ src/core/movegen.cpp
@@ -45,17 +45,17 @@ namespace Movegen {
         // If castling rights are available for current color add castling moves to the list
         if (color == Color::WHITE){
             if(board.getCastlingRights()[0]){
-                allMoves.push_back(Move(Square::E1, Square::C1, 0b0010));
+                allMoves.push_back(Move(Square::E1, Square::G1, 0b0010));
             }
             if(board.getCastlingRights()[1]){
-                allMoves.push_back(Move(Square::E1, Square::G1, 0b0011));
+                allMoves.push_back(Move(Square::E1, Square::C1, 0b0011));
             }
         } else {
             if(board.getCastlingRights()[2]){
-                allMoves.push_back(Move(Square::E8, Square::C8, 0b0010));
+                allMoves.push_back(Move(Square::E8, Square::G8, 0b0010));
             }
             if(board.getCastlingRights()[3]){
-                allMoves.push_back(Move(Square::E8, Square::G8, 0b0011));
+                allMoves.push_back(Move(Square::E8, Square::C8, 0b0011));
             }
         }
         return allMoves;
@@ -326,15 +326,18 @@ namespace Movegen {
     }
 
     bool isLegalMove(Board &board, Move move){
+        // Check for castling
+        if(move.getMoveCode() == MoveCode::KING_CASTLE || move.getMoveCode() == MoveCode::QUEEN_CASTLE){
+            if (!isLegalCastle(board, move)){
+                return false;
+            }
+        }
+
+    
         Color colorTurn = board.getIsWhiteTurn()? Color::WHITE : Color::BLACK;
         board.makeMove(move);
         bool isLegal = !isCheck(board, colorTurn);
         board.unmakeMove(move);
-
-        // Extra check for castling
-        if(move.getMoveCode() == MoveCode::KING_CASTLE || move.getMoveCode() == MoveCode::QUEEN_CASTLE){
-            isLegal = isLegalCastle(board, move);
-        }
 
         return isLegal;
     }
