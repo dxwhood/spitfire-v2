7a703b32079328090e2086602fbbc68823fffd79
Damien Hood <damien.hood@hotmail.com>
2025-03-19 17:08:29 -0400

    add keyword args support

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,34 @@
-#include "core/board.h"
-#include "core/movegen.h"
-#include "core/game.h"
-#include "utils/display.h"
-#include "utils/input.h"
-#include "eval/evaluation.h"
-#include "search/search.h"
-#include "../tests/test_movegen.h"
-#include <iostream>
-#include <string>
-#include "eval/heuristics.h"
-#include "eval/eval_constants.h"
 #include "utils/uci.h"
-
+#include "core/game.h"  // Assuming startgame() is in game.h
+#include <iostream>
+#include <vector>
+#include <string>
 
 using namespace chess;
 
+int main(int argc, char* argv[]) {
+    bool consoleMode = false;
+    bool playAsWhite = true;  // Default: engine plays as White
 
+    // Convert command-line arguments into a vector for easy processing
+    std::vector<std::string> args(argv + 1, argv + argc);
 
-int main() {
-    uci::loop();
+    for (const std::string& arg : args) {
+        if (arg == "--console") {
+            consoleMode = true;
+        } else if (arg == "--black") {
+            playAsWhite = false;  // Engine plays as Black
+        } else if (arg == "--white") {
+            playAsWhite = true;   // Engine plays as White (redundant, but allowed)
+        }
+    }
+
+    if (consoleMode) {
+        Game game;
+        game.start(playAsWhite);
+    } else {
+        uci::loop();  // Start UCI mode if no arguments are given
+    }
 
     return 0;
 }
